733eaa7fadee81b04092f5785b67809949bec168
Damir Brekalo <damir.brekalo@trikoder.net>
2014-04-01 12:32:17 +0200

    Added lib files

---
+++ dist/baseView.js
@@ -0,0 +1,108 @@
+;(function($, Backbone){
+
+	$.wk = $.wk || {};
+	var getTemplateHandler = $.wk.getTemplate;
+
+	$.wk.baseView = Backbone.View.extend({
+
+		constructor: function(){
+
+			this.subviews = {};
+			this.noModelViewCounter = 0;
+
+			Backbone.View.apply(this, arguments);
+
+		},
+
+		/* Subviews handlers
+		************************************/
+
+		close: function(parentInitiated){
+
+			if ( !parentInitiated ) { this.trigger('manualClosing'); }
+
+			if ( this.hasSubviews() ){
+				_.each(this.subviews, function(view){ view.close('parentInitiated'); });
+			}
+
+			this.remove();
+
+		},
+
+		closeSubviews: function(){
+
+			if ( !this.hasSubviews() ){ return; }
+			_.each( this.subviews, function(view){ view.close('parentInitiated'); } );
+			this.subviews = {};
+
+		},
+
+		addSubview: function(view){
+
+			var self = this;
+
+			if ( view.model ) { this.subviews[view.model.cid] = view; }
+			else { this.subviews['noModelView' + (++this.noModelViewCounter)] = view; }
+
+			view.on('manualClosing', function(){ delete self.subviews[view.model.cid]; });
+
+			return view;
+
+		},
+
+		removeSubview: function(model){
+
+			if (!this.subviews[model.cid]) { return false; }
+
+			this.subviews[model.cid].close('parentInitiated');
+			delete this.subviews[model.cid];
+
+		},
+
+		getSubview: function( model ){
+
+			return this.subviews[model.cid] ? this.subviews[model.cid] : false;
+
+		},
+
+		hasSubviews: function(){
+
+			return _.size(this.subviews) > 0 ? true : false;
+
+		},
+
+		/* Loaders
+		************************************/
+
+		loadingOn: function(){
+
+			this.$loadingHtml = this.$loadingHtml || $('<div class="loader"><span class="graphics">Loading</span></div>').appendTo( this.$el );
+			this.$loading_html.addClass('on');
+
+		},
+
+		loadingOff: function(){
+
+			if ( this.$loadingHtml ) { this.$loadingHtml.removeClass('on'); }
+
+		},
+
+		/* Template handlers
+		************************************/
+		getTemplate: function(templateName, templatePath, callback){
+
+			var self = this;
+			this.templates = this.templates || {};
+
+			var deffered = getTemplateHandler(templatePath,function(compiledTemplate){
+				self.templates[templateName] = compiledTemplate;
+				callback && callback.call(self, compiledTemplate);
+			});
+
+			return deffered;
+
+		}
+
+	});
+
+})(jQuery, Backbone);
